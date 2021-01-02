0f5ec62e82d77aab5913fd53fddf5ee855071657
Nathan Nicolle <nathan-nicolle@orange.fr>
2021-01-02 15:11:55 +0100

    Test de modification du template de galerie

--- wp-content/themes/childtheme/page.php
+++ wp-content/themes/childtheme/page.php
@@ -16,13 +16,28 @@ get_header(); ?>
 		<div id="primary" class="content-area">
 			<div id="content" class="site-content" role="main">
 
+			<?php if ( have_posts() ) : ?>
+
+				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
 
-					<?php comments_template( '', true ); ?>
+				<?php endwhile; ?>
 
-				<?php endwhile; // end of the loop. ?>
+				<?php snaps_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+
+			<?php endif; ?>
 
 			</div><!-- #content .site-content -->
 		</div><!-- #primary .content-area -->
